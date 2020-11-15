e40861773600d3748fcdd8e549575e87017c3179
hosomi <kiyotake.hosomi@outlook.jp>
2020-11-15 14:51:27 +0900

    ng update from Angular10 to Angular11.

--- src/app/pages/profile/profile.component.spec.ts
+++ src/app/pages/profile/profile.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { HighlightModule } from 'ngx-highlightjs';
 import { hljsLanguages } from 'src/app/app.module';
 
@@ -8,7 +8,7 @@ describe('ProfileComponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ProfileComponent],
       imports: [
